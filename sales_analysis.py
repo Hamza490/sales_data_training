ec220252a2759a81425af7b3c82989cd41b8caba
Hamza <muhammadmhamza35@gmail.com>
2024-10-08 19:48:07 -0400

    Made 4 Nested Linear Regression Models W/ No Interactions and displayed their r^2

--- sales_analysis.py
+++ sales_analysis.py
@@ -28,11 +28,43 @@ print("Testing samples:", len(x_test))
 '''
 
 # Step 5: Build Initial Linear Regression Model (No Interactions)
-X_train = sm.add_constant(x_train)  # Add a constant term to the model (intercept)
-X_test = sm.add_constant(x_test)
 
+# Model 1 - All Variables
+x1_train = sm.add_constant(x_train)  # Add a constant term to the model (intercept)
+x1_test = sm.add_constant(x_test)
 # Fit the model using OLS (Ordinary Least Squares)
-model = sm.OLS(y_train, x_train).fit()
-
+model_1 = sm.OLS(y_train, x1_train).fit()
 # Print the summary of the model
-print(model.summary())
+print(model_1.summary())
+
+# Model 2- Price
+x2_train = sm.add_constant(x_train['Price'])  # Add a constant term to the model (intercept)
+x2_test = sm.add_constant(x_test)
+# Fit the model using OLS (Ordinary Least Squares)
+model_2 = sm.OLS(y_train, x2_train).fit()
+# Print the summary of the model
+print(model_2.summary())
+
+# Model 3 - Price, Print_ads
+x3_train = sm.add_constant(x_train[['Price', 'Print_ads']])  # Add a constant term to the model (intercept)
+x3_test = sm.add_constant(x_test)
+# Fit the model using OLS (Ordinary Least Squares)
+model_3 = sm.OLS(y_train, x3_train).fit()
+# Print the summary of the model
+print(model_3.summary())
+
+# Model 4 - Price, Print_ads, online_ads
+x4_train = sm.add_constant(x_train[['Price', 'Print_ads', 'online_ads']])  # Add a constant term to the model (intercept)
+x4_test = sm.add_constant(x_test)
+# Fit the model using OLS (Ordinary Least Squares)
+model_4 = sm.OLS(y_train, x4_train).fit()
+# Print the summary of the model
+print(model_4.summary())
+
+# Model 4 - Price, Print_ads, online_ads
+x4_train = sm.add_constant(x_train[['Price', 'Print_ads', 'online_ads']])  # Add a constant term to the model (intercept)
+x4_test = sm.add_constant(x_test)
+# Fit the model using OLS (Ordinary Least Squares)
+model_4 = sm.OLS(y_train, x4_train).fit()
+# Print the summary of the model
+print(model_4.summary())
