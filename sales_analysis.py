6819c5ccfbcfd5a2dbed53e9e78f1d4c17cb9dfa
Hamza <muhammadmhamza35@gmail.com>
2024-10-08 17:43:01 -0400

    Created dataframe with dataset Printed dataframe to ensure all data is uploaded

--- sales_analysis.py
+++ sales_analysis.py
@@ -5,3 +5,8 @@ import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+
+# Step 2: Load Dataset into Pandas DataFrame
+dataFrame = pd.read_csv('Sales_Data.csv')
+
+print(dataFrame)
