8c0701f72d61a09c11c71089b6cccb4ab6251d6f
Hamza <muhammadmhamza35@gmail.com>
2024-10-08 19:07:45 -0400

    Created model 1 with all variables and 0 interactions.

--- sales_analysis.py
+++ sales_analysis.py
@@ -19,6 +19,20 @@ x = dataFrame[['Price', 'Print_ads', 'online_ads', 'TV_ads']]
 y = dataFrame['Sales']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+
+'''
+Tests for Step 4 (ensuring the data is split as expected)
 print("Total samples:", len(x))
 print("Training samples:", len(x_train))
 print("Testing samples:", len(x_test))
+'''
+
+# Step 5: Build Initial Linear Regression Model (No Interactions)
+X_train = sm.add_constant(x_train)  # Add a constant term to the model (intercept)
+X_test = sm.add_constant(x_test)
+
+# Fit the model using OLS (Ordinary Least Squares)
+model = sm.OLS(y_train, x_train).fit()
+
+# Print the summary of the model
+print(model.summary())
