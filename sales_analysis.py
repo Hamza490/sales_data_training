b60b04eceb1625dd122702392d54282441349e3c
Hamza <muhammadmhamza35@gmail.com>
2024-10-08 18:12:15 -0400

    Split Data into Test & Train
    Note: I am printing test and train data to verify
    the split is what I expect. This test will be commented
    out as I continue to build.

--- sales_analysis.py
+++ sales_analysis.py
@@ -9,5 +9,16 @@ import matplotlib.pyplot as plt
 # Step 2: Load Dataset into Pandas DataFrame
 dataFrame = pd.read_csv('Sales_Data.csv')
 
-print(dataFrame.info()) #Checks to see if we have any blank cells in our dataset
-print(dataFrame.describe()) #Churns out high level stas from your dataset
+# Step 3: Data Cleaning & High Level Stats
+#print(dataFrame.info()) #Checks to see if we have any blank cells in our dataset
+#print(dataFrame.describe()) #Churns out high level stas from your dataset
+
+# Step 4: Splitting dataset into sales_test and sales_train
+print(dataFrame.columns)
+x = dataFrame[['Price', 'Print_ads', 'online_ads', 'TV_ads']]
+y = dataFrame['Sales']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+print("Total samples:", len(x))
+print("Training samples:", len(x_train))
+print("Testing samples:", len(x_test))
