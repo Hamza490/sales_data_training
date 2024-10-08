8f0c2a2073dafdece839463c7987ed5cf14ba43a
Hamza <muhammadmhamza35@gmail.com>
2024-10-08 17:26:18 -0400

    Imported libraries to sales_analysis.py

---
+++ sales_analysis.py
@@ -0,0 +1,7 @@
+# Step 1 - Import Libraries
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
